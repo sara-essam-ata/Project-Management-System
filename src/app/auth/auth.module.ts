282d18af06dc8258b3c24ce847f58edac67035d1
sara-essam-ata <samsarh194@gmail.com>
2023-12-12 18:43:42 +0200

    global

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent}
+  { path: '', redirectTo: 'login', pathMatch:'full'},
+  { path: 'login', component: LoginComponent}
 ];
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
   ],
   declarations: [AuthComponent, LoginComponent]
 })
