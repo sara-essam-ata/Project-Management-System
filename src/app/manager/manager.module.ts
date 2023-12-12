02d8d796cf350f48f0326f1b1db7a7bcc7bde8dd
sara-essam-ata <samsarh194@gmail.com>
2023-12-12 05:52:17 +0200

    first commit

---
+++ src/app/manager/manager.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ManagerComponent } from './manager.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ManagerComponent]
+})
+export class ManagerModule { }
