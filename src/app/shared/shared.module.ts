0b014b2de35658d5a3496acea096f7fe17aac489
sara-essam-ata <samsarh194@gmail.com>
2023-12-12 18:42:10 +0200

    global

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponent } from './shared.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule, MatFormFieldModule, MatButtonModule, MatIconModule
+  ],
+  exports: [
+    MatInputModule, MatFormFieldModule, MatButtonModule, MatIconModule
   ],
   declarations: [SharedComponent]
 })
