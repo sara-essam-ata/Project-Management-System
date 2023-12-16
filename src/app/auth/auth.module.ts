437bbc022c2155e2d8e425662e5fa767ffc1e8c3
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2023-12-16 15:42:55 +0200

    added routes for register

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent}
+  {path:'', redirectTo: "login", pathMatch: "full"},
+  { path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
 ];
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [AuthComponent, LoginComponent]
+  declarations: [AuthComponent, LoginComponent, RegisterComponent]
 })
 export class AuthModule { }
