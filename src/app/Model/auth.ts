2daa07c9e56c836c99e8ece021f39afba24dab7f
sara-essam-ata <samsarh194@gmail.com>
2023-12-19 02:23:23 +0200

    change password

--- src/app/Model/auth.ts
+++ src/app/Model/auth.ts
@@ -2,3 +2,10 @@ export interface ILogin {
     email: string,
     password: string
 }
+export interface IChangePassword {
+    oldPassword: string,
+    newPassword: string,
+    confirmNewPassword: string,
+}
+
+
--- src/app/auth/components/changepassword/changepassword.component.ts
+++ src/app/auth/components/changepassword/changepassword.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../Services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-changepassword',
@@ -6,6 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./changepassword.component.scss']
 })
 export class ChangepasswordComponent {
-  hide:boolean=false;
+  hide: boolean = true;
+  constructor(private _AuthService:AuthService,
+    private _toastr:ToastrService,
+    private router:Router
+    ){}
+  changePasswordForm = new FormGroup({
+    oldPassword: new FormControl(null,[Validators.required,Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$')]),
+    newPassword: new FormControl(null,[Validators.required,Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$')]),
+    confirmNewPassword: new FormControl(null,[Validators.required,Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$')]),  
+  });
+
+  onSubmit(data: FormGroup){
+    this._AuthService.onChangePssword(data.value).subscribe({
+      next:(res)=>{
+        console.log(res);
+      },error:(err)=>{
+        this._toastr.error(err.error.messsage, 'error')
+      },complete:()=>{
+        this._toastr.success('password updated login now!', 'success');
+        this.router.navigate(['/auth'])
+      }
+    })
+  }
 
 }
