3e94bc885747cffdb77c692101c80f416702e6e9
Ahmed-Adel33 <ahmed920adel@gmail.com>
2023-12-20 13:47:39 +0200

    Add project

--- src/app/manager/manager-projects/components/add-update-project/add-update-project.component.ts
+++ src/app/manager/manager-projects/components/add-update-project/add-update-project.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ProjectsService } from '../../services/projects.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-update-project',
@@ -6,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-update-project.component.scss']
 })
 export class AddUpdateProjectComponent implements OnInit {
+projectForm=new FormGroup({
+  title:new FormControl(null),
+  description:new FormControl(null),
+})
+  constructor(private _ProjectsService:ProjectsService,private tostar:ToastrService,private router:Router) { }
+onSubmit(data:FormGroup){
+  console.log(data.value);
+  this._ProjectsService.onAddProject(data.value).subscribe({
+    next:(res)=>{
+      console.log(res);
+      
+    },error:(err)=>{
+      this.tostar.error(err.error.message,'Error');
+    },complete:()=>{
+      this.router.navigate(['dashboard/manager/projects'])
+    }
+  })
 
-  constructor() { }
-
+  
+}
   ngOnInit() {
   }
 
