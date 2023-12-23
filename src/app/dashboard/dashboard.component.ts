364431d9b8ae176617d2a4fe66ac1c9484bfe4c6
Ahmed-Adel33 <ahmed920adel@gmail.com>
2023-12-23 22:09:06 +0200

    first

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  receivedBoolean: boolean = true;
 
   constructor() { }
 
+  handleBooleanChange(value: boolean) {
+    this.receivedBoolean = value;
+  }
   ngOnInit() {
   }
+
+  
 
 }
