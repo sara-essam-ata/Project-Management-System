49b6141a936c2141ea65d154da49d7b2db68f0e6
sara-essam-ata <samsarh194@gmail.com>
2023-12-23 10:07:02 +0200

    users list

--- src/app/shared/logout/logout.component.ts
+++ src/app/shared/logout/logout.component.ts
@@ -9,8 +9,6 @@ import { Router } from '@angular/router';
 })
 export class LogoutComponent implements OnInit {
 
-  router: any;
-
   constructor( public dialogRef: MatDialogRef<LogoutComponent>,
     private Router:Router
      ) {}
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -44,7 +44,7 @@ export class SidebarComponent implements OnInit {
    },
    {
      title: 'Users',
-     icon: 'fa-solid fa-layer-group',
+     icon: 'fa-solid fa-users',
      link: '/dashboard/manager/users',
      isActive: this.isManager()
    },
@@ -63,15 +63,21 @@ export class SidebarComponent implements OnInit {
    {
      title: 'Projects',
      icon: 'fa-solid fa-calendar-day',
-     link: '/dashboard/user/recipes',
+     link: '/dashboard/employee/recipes',
      isActive: this.isEmployee()
    },
    {
      title: 'Tasks',
      icon: 'fa-solid fa-heart',
-     link: '/dashboard/user/favourites',
+     link: '/dashboard/employee/favourites',
      isActive: this.isEmployee()
-   }
+   },
+   {
+    title: 'Tasks',
+    icon: 'fa-solid fa-heart',
+    link: '/dashboard/employee/favourites',
+    isActive: this.isEmployee()
+  },
  ]
  
 
