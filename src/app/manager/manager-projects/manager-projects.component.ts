ce56fc038af1f1e891ac28779d0d4ff638d71df6
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2023-12-24 10:08:28 +0200

    view project ui & integration

--- src/app/manager/manager-projects/manager-projects.component.ts
+++ src/app/manager/manager-projects/manager-projects.component.ts
@@ -5,6 +5,7 @@ import { DeleteDialogComponent } from 'src/app/shared/delete-dialog/delete-dialo
 import { MatDialog } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 import { ViewProjectComponent } from './components/view-project/view-project.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manager-projects',
@@ -18,11 +19,13 @@ export class ManagerProjectsComponent implements OnInit {
     private _ProjectsService:ProjectsService,
     private dialog: MatDialog,
     private toastr: ToastrService,
+    private router:Router,
   ) { }
 
   ngOnInit() {
     this.getMyProjects()
   }
+
   getMyProjects() {
     this._ProjectsService.onGetManagerProjects().subscribe({
       next: (res) => {
@@ -43,11 +46,12 @@ export class ManagerProjectsComponent implements OnInit {
       console.log('The dialog was closed');
       if (result) {
         console.log(result.id);
-        this.onDeleteCategory(result.id);
+        this.onDeleteProject(result.id);
       }
     });
   }
-  onDeleteCategory(id: number) {
+
+  onDeleteProject(id: number) {
     this._ProjectsService.deleteProject(id).subscribe({
       next: (res) => {
         console.log(res);
@@ -59,22 +63,21 @@ export class ManagerProjectsComponent implements OnInit {
       }
     })
   }
-  
-  // openDeleteProject(projectData:any): void {
-  //   console.log(projectData);
-    
-  //   const dialogRef = this.dialog.open(DeleteDialogComponent, {
-  //     data: projectData,
-  //     width:'40%'
-  //   });
 
-  //   dialogRef.afterClosed().subscribe(result => {
-  //     console.log('The dialog was closed');
-  //     // console.log(result);
-  //     if (result) {
-  //       this.getMyProjects();
-  //     }
-  //   });
-  // }
+    // View
+    openViewDialog(listProjects: any): void {
+      const dialogRef = this.dialog.open(ViewProjectComponent, {
+        data: listProjects,
+        width: '60%',
+      });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+        this.router.navigate(['/dashboard/manager/projects'])
+      });
+    }
 
 }
+
+
+
