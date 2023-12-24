edfcb3387f87ecd881e074ec6df212f4e8679c7d
sara-essam-ata <samsarh194@gmail.com>
2023-12-24 23:37:26 +0200

    spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { GlobalInterceptor } from './interceptors/global.interceptor';
 import { ToastrModule } from 'ngx-toastr';
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { NgxSpinnerModule } from "ngx-spinner";
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -17,20 +21,30 @@ import { ToastrModule } from 'ngx-toastr';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    NgxSpinnerModule,
     HttpClientModule,
+    NgxSpinnerModule.forRoot
+    ({ type: 'ball-scale-multiple' }),
     ToastrModule.forRoot({
       closeButton: true,
       timeOut: 1000,
       progressBar: true,
     }),
+    
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: GlobalInterceptor,
       multi: true
+    },
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:LoadingInterceptor,
+      multi: true
     }
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
