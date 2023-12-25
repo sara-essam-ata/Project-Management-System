13eaf7d345f9d7f2548707c3cb851cdecd1d2fc2
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2023-12-25 13:42:16 +0200

    added view user UI and integration

--- src/app/manager/users/users.module.ts
+++ src/app/manager/users/users.module.ts
@@ -4,9 +4,12 @@ import { UsersComponent } from './users.component';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BlockUserComponent } from './components/block-user/block-user.component';
+import { ViewUserComponent } from './components/view-user/view-user.component';
 
 const routes:Routes = [
-  {path:'', component:UsersComponent}
+  {path:'', component:UsersComponent},
+  {path:'view/:id', component: ViewUserComponent}
+
   ]
 
 @NgModule({
@@ -15,6 +18,6 @@ const routes:Routes = [
     RouterModule.forChild(routes),
     SharedModule,
   ],
-  declarations: [UsersComponent,BlockUserComponent]
+  declarations: [UsersComponent,BlockUserComponent, ViewUserComponent]
 })
 export class UsersModule { }
