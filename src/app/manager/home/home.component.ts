a28bc0e8875e338f191b6c14fc17fdcdaf411d00
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2023-12-25 18:42:56 +0200

    Employee arch. & routing

--- src/app/manager/home/home.component.ts
+++ src/app/manager/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HelperService } from 'src/app/services/helper.service';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  userName = localStorage.getItem('userName')
-  constructor() { }
+  chart: any = [];
+  userCount:any;
 
-  ngOnInit() {
+  userName = localStorage.getItem('userName');
+  tasksCount = localStorage.getItem('TasksCount')
+  projectsCount = localStorage.getItem('projectsCount')
+
+  constructor(private _helperService:HelperService) { }
+
+  ngOnInit(): void {
+    this.grtTaskCount()
   }
+
+  grtTaskCount(){
+    this._helperService.getTaskCount().subscribe({
+      next: (res)=>{
+        console.log(res);
+        this.userCount = res;
+        // console.log(this.taskCount?.toDo, this.taskCount?.inProgress, this.taskCount?.done);
+   
+      }, error: (err)=>{
+        console.log(err);
+        
+      }, complete: ()=>{
+        this.chart = new Chart('canvas', {
+          type: 'doughnut',
+          data: {
+            labels: [
+              'To Do',
+              'In Progress',
+              'Done'
+            ],
+            datasets: [{
+              label: 'My First Dataset',
+              data: [this.userCount?.toDo, this.userCount?.inProgress, this.userCount?.done],
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+              ],
+              hoverOffset: 4
+            }]
+          }
+        })
+      }
+    })
+  }
+
 
 }
