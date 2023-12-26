2a198ed2fccc9f4ecd74b2181767f979210dd492
sara-essam-ata <samsarh194@gmail.com>
2023-12-26 08:29:51 +0200

    employee projects

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -3,24 +3,26 @@ import { CommonModule } from '@angular/common';
 import { EmployeeComponent } from './employee.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
  const routes:Routes=[
 {path:'',redirectTo:'home',pathMatch:'full'},
 {path:'home',component:HomeComponent},
 {
   path: 'projects',
-  loadChildren: () => import('./components/Employee-projects/Employee-projects.module').then(m => m.EmployeeProjectsModule)
+  loadChildren: () => import('./Employee-projects/Employee-projects.module').then(m => m.EmployeeProjectsModule)
 },
 {
   path: 'tasks',
-  loadChildren: () => import('./components/Employee-tasks/Employee-tasks.module').then(m => m.EmployeeTasksModule)
+  loadChildren: () => import('./Employee-tasks/Employee-tasks.module').then(m => m.EmployeeTasksModule)
 },
 
  ]
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
   ],
   declarations: [EmployeeComponent,HomeComponent]
 })
