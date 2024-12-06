69bf68a4bf10034f34c063a9b1dd0d543528ce72
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2024-12-06 22:12:06 +0200

    update

--- src/app/Guards/employee.guard.ts
+++ src/app/Guards/employee.guard.ts
@@ -21,30 +21,3 @@ export const employeeGuard: CanActivateFn = ( state) => {
 
 };
 
-
-
-// import { Injectable } from '@angular/core';
-// import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-// import { Observable } from 'rxjs';
-// import { AuthService } from '../auth/Services/auth.service';
-
-// @Injectable({
-//   providedIn:'root'
-// })
-// export class employeeGuard implements CanActivate{
-//   constructor(private router:Router, private _AuthService:AuthService){
-//     this._AuthService.getProfile()
-//   }
-//     canActivate(
-//       route: ActivatedRouteSnapshot,
-//       state: RouterStateSnapshot
-//     ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> |boolean | UrlTree {
-//       if(localStorage.getItem('userToken')!==null && localStorage.getItem('role')=='Employee'){
-//         return true;
-//       }
-//       else{
-//         this.router.navigate(['/dashboard']);
-//         return false;
-//       }
-//     }
-//   }
--- src/app/Guards/manager.guard.ts
+++ src/app/Guards/manager.guard.ts
@@ -20,28 +20,4 @@ export const managerGuard: CanActivateFn = ( state) => {
   }
 
 };
-// import { Injectable } from '@angular/core';
-// import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-// import { Observable } from 'rxjs';
-// import { AuthService } from '../auth/Services/auth.service';
 
-// @Injectable({
-//   providedIn:'root'
-// })
-// export class managerGuard implements CanActivate{
-//   constructor(private router:Router, private _AuthService:AuthService){
-//     this._AuthService.getProfile()
-//   }
-//     canActivate(
-//       route: ActivatedRouteSnapshot,
-//       state: RouterStateSnapshot
-//     ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> |boolean | UrlTree {
-//       if(localStorage.getItem('userToken')!==null && localStorage.getItem('role')=='Manager'){
-//         return true;
-//       }
-//       else{
-//         this.router.navigate(['/dashboard']);
-//         return false;
-//       }
-//     }
-//   }
